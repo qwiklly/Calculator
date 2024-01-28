cde8b05f80ee348a3607238247532fb153e66c93
Николай <@DESKTOP-05LAS6Q>
2024-01-28 12:23:15 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Linq.Expressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace kalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in MainRoot.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+
+            }
+        }
+
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private Calculator calculator = new Calculator();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            textLabel.Text += "";
+            switch (str)
+            {
+                case ("CE"):
+
+                    textLabel.Text = calculator.HandleCE();
+                    break;
+                case ("C"):
+                    textLabel.Text = calculator.HandleC();
+                    break;
+
+                case ("⌫"):
+                    textLabel.Text = calculator.HandleBackspace(textLabel.Text);
+                    break;
+                case ("+/-"):
+
+                    textLabel.Text = calculator.HandleNegative(textLabel.Text);
+                    break;
+                case ("x²"):
+
+                    textLabel.Text = calculator.HandlePow(textLabel.Text);
+                    break;
+                case ("1/x"):
+
+                    textLabel.Text = calculator.HandleInverse(textLabel.Text);
+                    break;
+                case ("√"):
+                    textLabel.Text = calculator.HandleSqrt(textLabel.Text);
+                    break;
+
+                case ("="):
+                    textLabel.Text = calculator.HandleEquals(textLabel.Text);
+                    break;
+                default:
+
+                    try
+                    {
+                        textLabel.Text += str;
+                    }
+                    catch
+                    {
+                        textLabel.Text = "error";
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ calculator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalculator
+{
+    public class Calculator
+    {
+        public string HandleCE()
+        {
+            return "";
+        }
+
+        public string HandleC()
+        {
+            return "";
+        }
+
+        public string HandleBackspace(string Text)
+        {
+            if (Text.Length > 0)
+            {
+                return Text.Substring(0, Text.Length - 1);
+            }
+            return Text;
+        }
+
+        public string HandleNegative(string Text)
+        {   if (Text.Length > 0)
+            {
+                double value1 = (-1) * double.Parse(Text);
+                return value1.ToString();
+            }
+        return Text;
+        }
+        public string HandlePow(string Text)
+        {
+            double value1 = Math.Pow(double.Parse(Text), 2);
+            return value1.ToString();
+        }
+
+        public string HandleInverse(string Text)
+        {
+            double value1 = 1 / double.Parse(Text);
+            return value1.ToString();
+        }
+
+        public string HandleSqrt(string Text)
+        {
+            double value1 = Math.Sqrt(double.Parse(Text));
+            return value1.ToString();
+        }
+ 
+        public string HandleEquals(string Text)
+        {
+
+
+            try
+            {
+
+                if (Text.Contains("x"))
+                {
+                    string[] values = SplitIntoTwo(Text, 'x');
+                    double result = Double.Parse(values[0]) * Double.Parse(values[1]);
+                    return result.ToString();
+
+                }
+                else if (Text.Contains("÷"))
+                {
+                    string[] values = SplitIntoTwo(Text, '÷');
+                    double result = Double.Parse(values[0]) / Double.Parse(values[1]);
+                    return result.ToString();
+                }
+                else if (Text.Contains("%"))
+                {
+                    string[] values = SplitIntoTwo(Text, '%');
+                    double result = Double.Parse(values[0]) * Double.Parse(values[1]) / 100;
+                    return result.ToString();
+                }
+                else if (Text.Contains("+"))
+                {
+                    string[] values = SplitIntoTwo(Text, '+');
+                    double result = Double.Parse(values[0]) + Double.Parse(values[1]);
+                    return result.ToString();
+                }
+                else if (Text.Contains("-"))
+                {
+                    string[] values = SplitIntoTwo(Text, '-');
+                    double result = Double.Parse(values[0]) - Double.Parse(values[1]);
+                    return result.ToString();
+
+                }
+            }
+            catch { return "error"; }
+            return "";
+        }
+
+        
+        private string[] SplitIntoTwo(string Text, char calculationMethod)
+        {
+            return Text.Split(calculationMethod);
+        }
+
+
+
+    }
+}
+
